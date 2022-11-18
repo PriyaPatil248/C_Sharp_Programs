8447787e7358c43599f8fa8a2a7a5423ee821119
PriyaPatil248 <114169890+PriyaPatil248@users.noreply.github.com>
2022-11-18 22:06:04 +0530

    Program on Singal Inheritance

---
+++ Singleinheritance/Singleinheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singleinheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer P1 = new Programmer();
+            Console.WriteLine("Salary:" + P1.salary);
+            Console.WriteLine("Bonus:" + P1.bonus);
+            Console.ReadLine();
+        }
+        public class Employee
+        {
+            public float salary = 70000;
+        }
+        public class Programmer : Employee
+        {
+            public float bonus = 40000;
+
+        }
+    }
+}
