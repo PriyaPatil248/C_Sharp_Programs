848096fbe3b015042a68ac0cfd07964ed766142b
PriyaPatil248 <114169890+PriyaPatil248@users.noreply.github.com>
2022-11-19 18:46:55 +0530

    Program on Multilevel Inheritance

---
+++ Multilevelinheritance/Multilevelinheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Multilevelinheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C obj = new C();
+            obj.show();
+            obj.display();
+            obj.greet();
+            Console.ReadLine();
+        }
+        class A
+        {
+            public void show()
+            {
+                Console.WriteLine("Hello from class A:Batch 1");
+            }
+        }
+        class B : A
+        {
+            public void display()
+            {
+                Console.WriteLine("Hello from class B:Batch 2");
+            }
+        }
+        class C : B
+        {
+            public void greet()
+            {
+                Console.WriteLine("Hello from class C:Batch 1 & 2");
+            }
+
+        }
+    }
+}
