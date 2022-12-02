b49eaeaf97cfec5bde97368c7a3a34b7d2b49487
PriyaPatil248 <114169890+PriyaPatil248@users.noreply.github.com>
2022-12-02 23:12:08 +0530

    File Exception

---
+++ FileException/FileException/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FileException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader("shivraj1.txt"))
+                {
+                    reader.ReadToEnd();
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
