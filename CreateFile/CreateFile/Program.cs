29f2a727e3fcd50aad61eba0ccc14053a7788e29
PriyaPatil248 <114169890+PriyaPatil248@users.noreply.github.com>
2022-12-03 18:41:28 +0530

    Create file to print 1 to 20

---
+++ CreateFile/CreateFile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CreateFile
+{
+    class CreateFile
+    {
+        static void Main(string[] args)
+        {
+            FileStream F = new FileStream("Test.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            for (int i = 1; i <= 20; i++)
+            {
+                F.WriteByte((byte)i);
+            }
+            F.Position = 0;
+            for (int i = 0; i <= 20; i++)
+            {
+                Console.Write(F.ReadByte() + " ");
+            }
+            F.Close();
+            Console.ReadKey();
+        }
+    }
+
+}
