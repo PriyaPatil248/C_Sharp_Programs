d8b4bccdf32940c01b00d8f3d98d0fae8818e486
PriyaPatil248 <114169890+PriyaPatil248@users.noreply.github.com>
2022-12-07 19:13:38 +0530

    User define Exception

---
+++ UserDefineException/UserDefineException/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserDefineException
+{
+    class UserDefineException : Exception
+    {
+        public UserDefineException(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+    class Program2
+    {
+        static void Main()
+        {
+            UserDefineException M = new UserDefineException("User define Exception");
+            try
+            {
+                throw M;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
